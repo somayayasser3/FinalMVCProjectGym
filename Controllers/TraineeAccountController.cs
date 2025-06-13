8a982faf1833b0bad2b3efbe6638645958d5692d
Moatasem-x <moatasememad55555@gmail.com>
2025-06-14 00:19:11 +0300

    Added Temp tany

--- Controllers/TraineeAccountController.cs
+++ Controllers/TraineeAccountController.cs
@@ -85,7 +85,7 @@ namespace GymManagementSystem.Controllers
                     IdentityResult role = await userManager.AddToRoleAsync(user, "Trainee");
                     List<Claim> claims = new List<Claim>();
                     claims.Add(new Claim("Displayname", userFromReq.FullName));
-                    claims.Add(new Claim("Role", "Admin"));
+                    claims.Add(new Claim("Role", "Trainee"));
                     await signInManager.SignInWithClaimsAsync(user, false, claims);
                     return RedirectToAction("Index", "Home");
                 }
